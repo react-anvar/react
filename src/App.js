6e468e0c8b4b18b925ffbe8ac79871b55074ac2a
react-anvar <react-anvar@gmail.com>
2023-06-09 19:05:35 +0500

    practice

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar';
+import NavbarStyle from './components/navbar.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    <Navbar logo='' link=''/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div id="nav">
+            <h1>salom dunyo</h1>
+        </div>
+    )
+}
+export default Navbar;
